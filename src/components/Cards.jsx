39913d5788837c1b01c2cef35e65f891c6079aa9
Priyabrata Das <priyobapi@gmail.com>
2024-01-03 23:28:13 +0530

    Replacing the localhost

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -26,7 +26,7 @@ const Cards = ({ item }) => {
     if(user && user.email){
         const cartItem = {menuItemId: _id, name, quantity : 1, image, price, email: user.email}
 
-        axios.post('http://localhost:5000/carts', cartItem)
+        axios.post('https://food-delivery-app-t6wc.onrender.com/carts', cartItem)
         .then((response) => {
           console.log(response);
           if(response){
--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://food-delivery-app-t6wc.onrender.com'
 })
 
 const useAxiosPublic = () => {
